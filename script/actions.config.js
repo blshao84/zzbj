db50ff91e3cd83866f7447dfebbd66c4b380c4d6
Lei Shi <sinorockie@gmail.com>
2021-01-07 15:28:26 +0800

    调整IP

--- script/actions.config.js
+++ script/actions.config.js
@@ -4,9 +4,7 @@ module.exports = {
         script: 'java',
         args: ['-jar', '-Xms128m', '-Xmx2g', 'bin/center-1.0.jar'],
         env: {
-            FILE_ANSWER_LOCATION: './answer-document',
-            FILE_BASE_LOCATION: './base-document',
-            FILE_ATTACHMENT_LOCATION: './attachment-document'
+            POSTGRES_HOST: '192.168.99.100'
         }
     }]
 };
